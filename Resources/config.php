b5316e6404a97d7c20f8be6a9cae755658601dda
winlilom <ducnhat2425@gmail.com>
2025-11-13 19:15:14 +0700

    database

--- Resources/config.php
+++ Resources/config.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'host' => 'localhost',
-    'db_name' => 'ten_csdl',
-    'username' => 'root',
-    'password' => ''
+    'host' => 'localhost',          // máy chủ MySQL (mặc định: localhost)
+    'db_name' => 'QUANLY_NHAHANG',  // tên CSDL bạn vừa tạo
+    'username' => 'root',           // tài khoản MySQL (mặc định của XAMPP/WAMP)
+    'password' => ''                // mật khẩu MySQL (thường để trống trong localhost)
 ];
